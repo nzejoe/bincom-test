700b7db33ec9a155b8b6ffa61e8448faaec5e236
nzejoe <jonathannzete@gmail.com>
2023-01-24 11:56:19 +0100

    AnnouncedPuResults model created

--- polls/models.py
+++ polls/models.py
@@ -22,3 +22,19 @@ class AnnouncedLgaResults(models.Model):
     entered_by_user = models.CharField(max_length=50)
     date_entered = models.DateTimeField(default=now)
     user_ip_address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.lga_name
+
+
+class AnnouncedPuResults(models.Model):
+    result_id = models.AutoField(auto_created=True, primary_key=True)
+    polling_unit_uniqueid = models.CharField(max_length=50)
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField(default=now)
+    user_ip_address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.polling_unit_uniqueid
