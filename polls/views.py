a6207ff4b6ae351d78a5fbb0c496783ef7a3feff
nzejoe <jonathannzete@gmail.com>
2023-01-25 00:51:42 +0100

    home page data rendering

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import PollingUnit
+
 
 def home(request):
-    return render(request, "home.html")
+    polling_units = PollingUnit.objects.all()
+    print()
+    print(polling_units)
+    print()
+    context = {
+        "polling_units": polling_units,
+    }
+    return render(request, "home.html", context)
